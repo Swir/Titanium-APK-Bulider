4acc3f6c01302d742effd4ae6a4b75ef83c48cd9
Jan Szczerbaty Uśmiechnięty <swirjestem@hotmail.com>
2026-09-08 08:59:15 +0200

    SWIR Czateria: add Android app module

---
+++ SWIR_Czateria_Android/app/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "pl.swir.czateria"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "pl.swir.czateria"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.2.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
