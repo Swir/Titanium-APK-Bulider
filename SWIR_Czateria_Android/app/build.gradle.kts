c1879400749e96a4e250221465c63f05f7896c5f
Jan Szczerbaty Uśmiechnięty <swirjestem@hotmail.com>
2026-09-08 10:01:37 +0200

    Bump SWIR Czateria to v0.5

--- SWIR_Czateria_Android/app/build.gradle.kts
+++ SWIR_Czateria_Android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "pl.swir.czateria"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
     }
 
     buildTypes {
